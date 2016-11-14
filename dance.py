d8e2f3524389a68c4cdc588b41cfde26d68d4e28
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-14 16:05:29 +0000

    Made async

--- dance.py
+++ dance.py
@@ -2,8 +2,8 @@ from opsdroid.skills import match_regex
 import random
 
 @match_regex(r'.*dance.*')
-def dance(opsdroid, message):
+async def dance(opsdroid, message):
     gifs = ["http://i.imgur.com/lnvJm9a.gif",
             "http://i.imgur.com/DyzcWlX.gif",
             "http://i.imgur.com/Cj7pp8k.gif"]
-    message.respond(random.choice(gifs))
+    await message.respond(random.choice(gifs))
