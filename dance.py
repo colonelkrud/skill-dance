e60a97bca1f6392893bc86964f614bf81e475b66
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2017-01-31 19:03:53 +0000

    Update for v0.6.0

--- dance.py
+++ dance.py
@@ -1,8 +1,8 @@
-from opsdroid.skills import match_regex
+from opsdroid.matchers import match_regex
 import random
 
 @match_regex(r'.*dance.*')
-async def dance(opsdroid, message):
+async def dance(opsdroid, config, message):
     gifs = ["http://i.imgur.com/lnvJm9a.gif",
             "http://i.imgur.com/DyzcWlX.gif",
             "http://i.imgur.com/Cj7pp8k.gif"]
