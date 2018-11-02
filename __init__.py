deb8716318d8fd301820ec1b76275ac2df354d7b
colonelkrud <admin@colonelkrud.com>
2018-11-02 13:18:42 -0400

    Update __init__.py
    match "dance" but not words containing "dance" like "guidance".

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from opsdroid.matchers import match_regex
 import random
 
-@match_regex(r'.*dance.*')
+@match_regex(r'\bdance\b', case_sensitive=False)
 async def dance(opsdroid, config, message):
     gifs = ["http://i.imgur.com/lnvJm9a.gif",
             "http://i.imgur.com/DyzcWlX.gif",
